cec78f48f897603c8a74b098eb28ed58ae2d71da
Silver-Runner <rishabhm142003@gmail.com>
2025-12-24 00:35:09 +0530

    updated the index file

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const { connectMongoDB } = require("./config/db-config");
 const cookieParser = require("cookie-parser");
 require('dotenv').config();
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://event-ease-mern-client-8bmb.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 // Connect to MongoDB
 connectMongoDB();
 
