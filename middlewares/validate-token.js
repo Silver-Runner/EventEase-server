dacc5d776e31795e97b6ff82c5014ae0a58db8b8
Silver-Runner <rishabhm142003@gmail.com>
2025-12-26 21:00:59 +0530

    updates for the token

--- middlewares/validate-token.js
+++ middlewares/validate-token.js
@@ -1,7 +1,13 @@
 const jwt = require("jsonwebtoken");
 const validateToken = (req,res,next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  
   try {
-    const token = req.cookies.token;
+    const token = authHeader.split(" ")[1];
     if(!token){
       return res.status(401).json({message: "Unauthorized"});
     }
